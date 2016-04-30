b2138751d5e76fab110bc35cc11545d2900aaf12
Joseph Garcia <j.garc419@gmail.com>
2016-04-30 01:50:39 -0400

    setup MVC structure/ constants file / allow arbitrary loads

---
+++ CineSearch/constants.swift
@@ -0,0 +1,23 @@
+//
+//  constants.swift
+//  CineSearch
+//
+//  Created by Joseph Garcia on 4/30/16.
+//  Copyright © 2016 joebeard. All rights reserved.
+//
+
+import Foundation
+
+// EXAMPLE API CALL
+// https://api.themoviedb.org/3/movie/550?api_key=34a46b49db5c44899a8ffe4bf365854b
+// API KEY
+// ?api_key=34a46b49db5c44899a8ffe4bf365854b
+
+
+//URLS
+let POPULAR_MOVIES_URL = "https://api.themoviedb.org/3/movie/popular?api_key=34a46b49db5c44899a8ffe4bf365854b"
+let SEARCH_MOVIES_URL = "https://api.themoviedb.org/3/search/movie?api_key=34a46b49db5c44899a8ffe4bf365854b&query="
+let MOVIE_POSTER_IMG_URL = "http://image.tmdb.org/t/p/w500"
+
+
+//SEGUES
