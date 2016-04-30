ef9da549f017ccc8211e40092ce3f14b4dfdc96b
Joseph Garcia <j.garc419@gmail.com>
2016-04-30 18:09:20 -0400

    created Home/Search/Detail VC's. Set up UI for VC's

---
+++ CineSearch/SearchViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SearchViewController.swift
+//  CineSearch
+//
+//  Created by Joseph Garcia on 4/30/16.
+//  Copyright © 2016 joebeard. All rights reserved.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
--- CineSearch/constants.swift
+++ CineSearch/constants.swift
@@ -16,6 +16,8 @@ import Foundation
 
 //URLS
 let POPULAR_MOVIES_URL = "https://api.themoviedb.org/3/movie/popular?api_key=34a46b49db5c44899a8ffe4bf365854b"
+let NOW_PLAYING_URL = "https://api.themoviedb.org/3/movie/now_playing?api_key=34a46b49db5c44899a8ffe4bf365854b"
+let TOP_RATED_URL = "https://api.themoviedb.org/3/movie/top_rated?api_key=34a46b49db5c44899a8ffe4bf365854b"
 let SEARCH_MOVIES_URL = "https://api.themoviedb.org/3/search/movie?api_key=34a46b49db5c44899a8ffe4bf365854b&query="
 let MOVIE_POSTER_IMG_URL = "http://image.tmdb.org/t/p/w500"
 
