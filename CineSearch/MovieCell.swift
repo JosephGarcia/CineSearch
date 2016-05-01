4f3abce468b55a74f445726499a61b78066e6190
Joseph Garcia <j.garc419@gmail.com>
2016-05-01 14:57:54 -0400

    made a configureCell function. Takes care of all the placeholders in the cell

--- CineSearch/MovieCell.swift
+++ CineSearch/MovieCell.swift
@@ -7,9 +7,31 @@
 //
 
 import UIKit
+import Spring
+import Kingfisher
 
-class MovieCell: UICollectionViewCell {
+class MovieCell: UICollectionViewCell{
     
     @IBOutlet weak var moviePoster: UIImageView!
     @IBOutlet weak var movieTitle: UILabel!
+    var movie: Movie!
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        layer.cornerRadius = 3.0
+        layer.masksToBounds = false;
+        layer.shadowColor = UIColor(red: SHADOW_COLOR, green: SHADOW_COLOR, blue: SHADOW_COLOR, alpha: 0.5).CGColor
+        layer.shadowOpacity = 0.8
+        layer.shadowRadius = 6.0
+        layer.shadowOffset = CGSizeMake(0.0, 3.0)
+    }
+    
+    func configureCell(movie: Movie){
+        self.movie = movie
+        let imageURL = NSURL(string:"\(TMDB_MOVIE_POSTER_IMG_URL)\(self.movie.imageURL)")
+        moviePoster.kf_setImageWithURL(imageURL!)
+        movieTitle.text = self.movie.title
+    }
+
 }
